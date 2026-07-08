4e715277c68853738a454251f559f897bebba97f
Michael Jacob <michaeljacobd2211@gmail.com>
2026-07-08 09:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.73%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,17 +2,12 @@ class Solution:
     def subsets(self, nums):
         b=[]
         def f(i,c):
-            nonlocal b
-
             if i==len(nums):
                 b.append(c[:])
-                return
-            
+                return        
             c.append(nums[i])
             f(i+1,c)
             c.pop()
             f(i+1,c)
-
         f(0,[])
-        b.sort()
         return b
